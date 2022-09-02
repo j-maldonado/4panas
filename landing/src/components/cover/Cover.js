494b3dc0abc378ad6e9f1beff21514482019a04d
Joan Maldonado <87048399+j-maldonado@users.noreply.github.com>
2022-09-02 12:00:12 -0300

    Inicio del proyecto 4panas

---
+++ landing/src/components/cover/Cover.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Cover.css'
+import coverVideo from '../cover/media/coverVideo.mp4'
+
+const Cover = () => {
+  return (
+    <div className='coverContainer'>
+        
+
+    </div>
+  )
+}
+
+export default Cover
