c134fe03e157b465e9425d43d203c6a9525d784c
Joan Maldonado <87048399+j-maldonado@users.noreply.github.com>
2022-09-02 17:47:58 -0300

    avances

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './Cover.css'
 import coverVideo from '../../media/coverVideo.mp4'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import oferta1 from '../../media/img/oferta1.png'
+import oferta2 from '../../media/img/oferta2.png'
+import oferta3 from '../../media/img/oferta3.png'
+
+
 
 const Cover = () => {
   return (
     <div className='cover-Container'>
-   
-      <video className="video" src={coverVideo} autoPlay loop muted></video>
+      <video className="video" src={coverVideo} autoPlay loop muted />
+      <Carousel className='carousel' autoPlay wrapAround={true} slidesToShow={3}>
+        <div className='carouselimg'>
+          <img src={oferta1} />
+        </div>
+        <div className='carouselimg'>
+          <img src={oferta2} />
+
+        </div>
+        <div className='carouselimg'>
+          <img src={oferta3} />
+
+        </div>
+      </Carousel>
+
 
     </div>
   )
