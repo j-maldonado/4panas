d60198c0f6107294d528b01c1c56619ebe6b97da
Joan-Maldonado <joan@seguroweb.com.ar>
2022-09-05 14:55:01 -0300

    update 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "./components/Navbar/Navbar"
 import Cover from "./components/cover/Cover";
-import logo from './media/img/4panas-Logo-noBG.png'
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}     
-              width="70"
-              height="70"
-              className="d-inline-block align-top"
-            />{' '}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-
+    <Navbar></Navbar>
     <Cover></Cover>
     </div>
   );
--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Cover.css'
 import coverVideo from '../../media/coverVideo.mp4'
-import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Carousel from 'react-bootstrap/Carousel';
 import oferta1 from '../../media/img/oferta1.png'
 import oferta2 from '../../media/img/oferta2.png'
 import oferta3 from '../../media/img/oferta3.png'
@@ -11,25 +11,26 @@ import oferta3 from '../../media/img/oferta3.png'
 
 const Cover = () => {
   return (
-    <div className='cover-Container'>
+    <div className="cover-Container">
       <video className="video" src={coverVideo} autoPlay loop muted />
-      <Carousel className='carousel' autoPlay wrapAround={true} slidesToShow={3}>
-        <div className='carouselimg'>
-          <img src={oferta1} />
-        </div>
-        <div className='carouselimg'>
-          <img src={oferta2} />
 
-        </div>
-        <div className='carouselimg'>
-          <img src={oferta3} />
+      <div className="carousel-container" >
+        <Carousel className="carousel" variant="dark" aria-hidden="false">
+          <Carousel.Item>
+            <img className="d-block-w-100" src={oferta1} alt="First slide" />
+          </Carousel.Item>
 
-        </div>
-      </Carousel>
+          <Carousel.Item>
+            <img className="d-block-w-100" src={oferta2} alt="Second slide" />
+          </Carousel.Item>
 
-
+          <Carousel.Item>
+            <img className="d-block-w-100" src={oferta3} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cover
