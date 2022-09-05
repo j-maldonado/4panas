808d5868efef93a3fde5951c7a4df7ac0d075b4e
Joan-Maldonado <joan@seguroweb.com.ar>
2022-09-05 15:32:50 -0300

    update 3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,18 +6,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../../media/img/4panas-Logo-noBG.png'
+import './Navbar.css'
+import {BiUserCircle} from 'react-icons/bi';
 
 
 const Navbarc = () => {
   return (
-    <Navbar variant='dark' bg="dark" expand="lg">
+    <Navbar variant="dark" bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#"><img src={logo} height="70"></img></Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img src={logo} height="70"></img>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-1"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <NavDropdown title="Productos" id="navbarScrollingDropdown">
@@ -27,7 +31,9 @@ const Navbarc = () => {
               <NavDropdown.Item href="#action4">Mouse</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Gabinetes</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Mothers</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Placas de Video</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Placas de Video
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">
                 Todos los productos
@@ -37,11 +43,13 @@ const Navbarc = () => {
             <Nav.Link href="#action2">¿Quienes somos?</Nav.Link>
             <Nav.Link href="#action2">Contactanos</Nav.Link>
           </Nav>
-          
         </Navbar.Collapse>
       </Container>
+      <button class="icon-btn add-btn">
+        <div class="add-icon"></div>
+        <div class="btn-txt"><BiUserCircle/></div>
+      </button>
     </Navbar>
-
-  )};
+  );};
 
 export default Navbarc;
